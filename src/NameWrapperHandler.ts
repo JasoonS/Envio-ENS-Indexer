34553ca1b7e62f039d395dc33dcc13f4a3d42c3e
Jason <jason@envio.dev>
2024-10-26 08:33:47 +0200

    Finish upgrade to v2

--- src/NameWrapperHandler.ts
+++ src/NameWrapperHandler.ts
@@ -2,39 +2,25 @@
  *Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features*
  */
 import {
-  NameWrapperContract_ExpiryExtended_handler,
-  NameWrapperContract_ExpiryExtended_loader,
-  NameWrapperContract_FusesSet_handler,
-  NameWrapperContract_FusesSet_loader,
-  NameWrapperContract_NameUnwrapped_handler,
-  NameWrapperContract_NameUnwrapped_loader,
-  NameWrapperContract_NameWrapped_handler,
-  NameWrapperContract_NameWrapped_loader,
-  NameWrapperContract_OwnershipTransferred_handler,
-  NameWrapperContract_OwnershipTransferred_loader,
-  NameWrapperContract_TransferBatch_handler,
-  NameWrapperContract_TransferBatch_loader,
-  NameWrapperContract_TransferSingle_handler,
-  NameWrapperContract_TransferSingle_loader
-} from "../generated/src/Handlers.gen";
+  NameWrapper,
+  Account,
+  Domain,
+  ExpiryExtended,
+  FusesSet,
+  NameUnwrapped,
+  NameWrapped,
+  NameWrapperEventsSummary,
+  OwnershipTransferred,
+  TransferBatch,
+  TransferSingle,
+  WrappedDomain
+} from "generated";
 
-import {
-  accountEntity,
-  ExpiryExtendedEntity,
-  FusesSetEntity,
-  NameUnwrappedEntity,
-  NameWrappedEntity,
-  NameWrapperEventsSummaryEntity,
-  OwnershipTransferredEntity,
-  TransferBatchEntity,
-  TransferSingleEntity,
-  wrappedDomainEntity
-} from "../generated/src/Types.gen";
 import { ETH_NODE } from "./utils";
 
 const GLOBAL_EVENTS_SUMMARY_KEY_2 = "GlobalEventsSummary";
 
-const INITIAL_EVENTS_SUMMARY: NameWrapperEventsSummaryEntity = {
+const INITIAL_EVENTS_SUMMARY: NameWrapperEventsSummary = {
   id: GLOBAL_EVENTS_SUMMARY_KEY_2,
   approvalsCount: BigInt(0),
   approvalForAllsCount: BigInt(0),
@@ -54,273 +40,268 @@ function checkPccBurned(fuses: bigint): boolean {
   return (fuses & PARENT_CANNOT_CONTROL) == PARENT_CANNOT_CONTROL;
 }
 
-NameWrapperContract_ExpiryExtended_loader(({ event, context }) => {
-  context.NameWrapperEventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY_2);
-  context.Domain.load(event.params.node, {});
-});
+NameWrapper.ExpiryExtended.handlerWithLoader({
+  loader: async ({ event, context }) => {
+    const summary = await context.NameWrapperEventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY_2);
+    const domain = await context.Domain.get(event.params.node);
+    return { summary, domain };
+  },
+  handler: async ({ event, context, loaderReturn }) => {
+    let { summary, domain } = loaderReturn;
 
-NameWrapperContract_ExpiryExtended_handler(({ event, context }) => {
-  let summary = context.NameWrapperEventsSummary.get(
-    GLOBAL_EVENTS_SUMMARY_KEY_2
-  );
+    let currentSummaryEntity: NameWrapperEventsSummary =
+      summary ?? INITIAL_EVENTS_SUMMARY;
 
-  let currentSummaryEntity: NameWrapperEventsSummaryEntity =
-    summary ?? INITIAL_EVENTS_SUMMARY;
-
-  let nextSummaryEntity = {
-    ...currentSummaryEntity,
-    expiryExtendedsCount: currentSummaryEntity.expiryExtendedsCount + BigInt(1)
-  };
-
-  let expiryExtendedEntity: ExpiryExtendedEntity = {
-    id: event.transactionHash + event.logIndex.toString(),
-    node: event.params.node,
-    expiry: event.params.expiry,
-    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY_2
-  };
-
-  context.NameWrapperEventsSummary.set(nextSummaryEntity);
-  context.ExpiryExtended.set(expiryExtendedEntity);
-
-  let domain = context.Domain.get(event.params.node);
-
-  if (domain !== undefined) {
-    domain = { ...domain, expiryDate: event.params.expiry };
-    context.Domain.set(domain);
-  }
-});
-
-NameWrapperContract_FusesSet_loader(({ event: _, context }) => {
-  context.NameWrapperEventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY_2);
-});
-
-NameWrapperContract_FusesSet_handler(({ event, context }) => {
-  let summary = context.NameWrapperEventsSummary.get(
-    GLOBAL_EVENTS_SUMMARY_KEY_2
-  );
-
-  let currentSummaryEntity: NameWrapperEventsSummaryEntity =
-    summary ?? INITIAL_EVENTS_SUMMARY;
-
-  let nextSummaryEntity = {
-    ...currentSummaryEntity,
-    fusesSetsCount: currentSummaryEntity.fusesSetsCount + BigInt(1)
-  };
-
-  let fusesSetEntity: FusesSetEntity = {
-    id: event.transactionHash + event.logIndex.toString(),
-    node: event.params.node,
-    fuses: event.params.fuses,
-    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY_2
-  };
-
-  context.NameWrapperEventsSummary.set(nextSummaryEntity);
-  context.FusesSet.set(fusesSetEntity);
-});
-
-NameWrapperContract_NameUnwrapped_loader(({ event, context }) => {
-  context.NameWrapperEventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY_2);
-  context.Domain.load(event.params.node, {});
-});
-
-NameWrapperContract_NameUnwrapped_handler(({ event, context }) => {
-  let summary = context.NameWrapperEventsSummary.get(
-    GLOBAL_EVENTS_SUMMARY_KEY_2
-  );
-
-  let currentSummaryEntity: NameWrapperEventsSummaryEntity =
-    summary ?? INITIAL_EVENTS_SUMMARY;
-
-  let nextSummaryEntity = {
-    ...currentSummaryEntity,
-    nameUnwrappedsCount: currentSummaryEntity.nameUnwrappedsCount + BigInt(1)
-  };
-
-  let nameUnwrappedEntity: NameUnwrappedEntity = {
-    id: event.transactionHash + event.logIndex.toString(),
-    node: event.params.node,
-    owner: event.params.owner,
-    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY_2
-  };
-
-  context.NameWrapperEventsSummary.set(nextSummaryEntity);
-  context.NameUnwrapped.set(nameUnwrappedEntity);
-
-  let domain = context.Domain.get(event.params.node);
-  if (domain !== undefined) {
-    domain = {
-      ...domain,
-      wrappedOwner_id: undefined
+    let nextSummaryEntity = {
+      ...currentSummaryEntity,
+      expiryExtendedsCount: currentSummaryEntity.expiryExtendedsCount + BigInt(1)
     };
 
-    if (domain.expiryDate && domain.parent_id !== ETH_NODE) {
-      domain = { ...domain, expiryDate: undefined };
-    }
-
-    context.Domain.set(domain);
-  }
-});
-
-NameWrapperContract_NameWrapped_loader(({ event, context }) => {
-  context.NameWrapperEventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY_2);
-  context.Domain.load(event.params.node, {});
-  context.Account.load(event.params.owner);
-});
-
-NameWrapperContract_NameWrapped_handler(({ event, context }) => {
-  let summary = context.NameWrapperEventsSummary.get(
-    GLOBAL_EVENTS_SUMMARY_KEY_2
-  );
-
-  let currentSummaryEntity: NameWrapperEventsSummaryEntity =
-    summary ?? INITIAL_EVENTS_SUMMARY;
-
-  let nextSummaryEntity = {
-    ...currentSummaryEntity,
-    nameWrappedsCount: currentSummaryEntity.nameWrappedsCount + BigInt(1)
-  };
-
-  let nameWrappedEntity: NameWrappedEntity = {
-    id: event.transactionHash + event.logIndex.toString(),
-    node: event.params.node,
-    name: event.params.name,
-    owner: event.params.owner,
-    fuses: event.params.fuses,
-    expiry: event.params.expiry,
-    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY_2
-  };
-
-  context.NameWrapperEventsSummary.set(nextSummaryEntity);
-  context.NameWrapped.set(nameWrappedEntity);
-
-  let domain = context.Domain.get(event.params.node);
-  let domainID = event.params.node;
-  let owner = context.Account.get(event.params.owner);
-
-  if (owner === undefined) {
-    owner = <accountEntity>{
-      id: event.params.owner
-    };
-    context.Account.set(owner);
-  }
-
-  if (domain !== undefined) {
-    domainID = domain.id;
-    domain = {
-      ...domain,
-      name: event.params.name,
-      owner_id: owner.id,
-      wrappedOwner_id: owner.id
+    let expiryExtendedEntity: ExpiryExtended = {
+      id: event.transaction.hash + event.logIndex.toString(),
+      node: event.params.node,
+      expiry: event.params.expiry,
+      eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY_2
     };
 
-    if (
-      checkPccBurned(event.params.fuses) &&
-      (!domain.expiryDate || event.params.expiry > domain.expiryDate!)
-    ) {
+    await context.NameWrapperEventsSummary.set(nextSummaryEntity);
+    await context.ExpiryExtended.set(expiryExtendedEntity);
+
+    if (domain !== undefined) {
       domain = { ...domain, expiryDate: event.params.expiry };
+      await context.Domain.set(domain);
+    }
+  }
+});
+
+NameWrapper.FusesSet.handlerWithLoader({
+  loader: async ({ event, context }) => {
+    const summary = await context.NameWrapperEventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY_2);
+    return { summary };
+  },
+  handler: async ({ event, context, loaderReturn }) => {
+    let { summary } = loaderReturn;
+
+    let currentSummaryEntity: NameWrapperEventsSummary =
+      summary ?? INITIAL_EVENTS_SUMMARY;
+
+    let nextSummaryEntity = {
+      ...currentSummaryEntity,
+      fusesSetsCount: currentSummaryEntity.fusesSetsCount + BigInt(1)
+    };
+
+    let fusesSetEntity: FusesSet = {
+      id: event.transaction.hash + event.logIndex.toString(),
+      node: event.params.node,
+      fuses: event.params.fuses,
+      eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY_2
+    };
+
+    await context.NameWrapperEventsSummary.set(nextSummaryEntity);
+    await context.FusesSet.set(fusesSetEntity);
+  }
+});
+
+NameWrapper.NameUnwrapped.handlerWithLoader({
+  loader: async ({ event, context }) => {
+    const summary = await context.NameWrapperEventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY_2);
+    const domain = await context.Domain.get(event.params.node);
+    return { summary, domain };
+  },
+  handler: async ({ event, context, loaderReturn }) => {
+    let { summary, domain } = loaderReturn;
+
+    let currentSummaryEntity: NameWrapperEventsSummary =
+      summary ?? INITIAL_EVENTS_SUMMARY;
+
+    let nextSummaryEntity = {
+      ...currentSummaryEntity,
+      nameUnwrappedsCount: currentSummaryEntity.nameUnwrappedsCount + BigInt(1)
+    };
+
+    let nameUnwrappedEntity: NameUnwrapped = {
+      id: event.transaction.hash + event.logIndex.toString(),
+      node: event.params.node,
+      owner: event.params.owner,
+      eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY_2
+    };
+
+    await context.NameWrapperEventsSummary.set(nextSummaryEntity);
+    await context.NameUnwrapped.set(nameUnwrappedEntity);
+
+    if (domain !== undefined) {
+      domain = {
+        ...domain,
+        wrappedOwner_id: undefined
+      };
+
+      if (domain.expiryDate && domain.parent_id !== ETH_NODE) {
+        domain = { ...domain, expiryDate: undefined };
+      }
+
+      await context.Domain.set(domain);
+    }
+  }
+});
+
+NameWrapper.NameWrapped.handlerWithLoader({
+  loader: async ({ event, context }) => {
+    const summary = await context.NameWrapperEventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY_2);
+    const domain = await context.Domain.get(event.params.node);
+    const owner = await context.Account.get(event.params.owner);
+    return { summary, domain, owner };
+  },
+  handler: async ({ event, context, loaderReturn }) => {
+    let { summary, domain, owner } = loaderReturn;
+
+    let currentSummaryEntity: NameWrapperEventsSummary =
+      summary ?? INITIAL_EVENTS_SUMMARY;
+
+    let nextSummaryEntity = {
+      ...currentSummaryEntity,
+      nameWrappedsCount: currentSummaryEntity.nameWrappedsCount + BigInt(1)
+    };
+
+    let nameWrappedEntity: NameWrapped = {
+      id: event.transaction.hash + event.logIndex.toString(),
+      node: event.params.node,
+      name: event.params.name,
+      owner: event.params.owner,
+      fuses: event.params.fuses,
+      expiry: event.params.expiry,
+      eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY_2
+    };
+
+    await context.NameWrapperEventsSummary.set(nextSummaryEntity);
+    await context.NameWrapped.set(nameWrappedEntity);
+
+    let domainID = event.params.node;
+
+    if (owner === undefined) {
+      owner = <Account>{
+        id: event.params.owner
+      };
+      await context.Account.set(owner);
     }
 
-    context.Domain.set(domain);
+    if (domain !== undefined) {
+      domainID = domain.id;
+      domain = {
+        ...domain,
+        name: event.params.name,
+        owner_id: owner.id,
+        wrappedOwner_id: owner.id
+      };
+
+      if (
+        checkPccBurned(event.params.fuses) &&
+        (!domain.expiryDate || event.params.expiry > domain.expiryDate!)
+      ) {
+        domain = { ...domain, expiryDate: event.params.expiry };
+      }
+
+      await context.Domain.set(domain);
+    }
+
+    let wrappedDomain: WrappedDomain = {
+      domain_id: domainID,
+      expiryDate: event.params.expiry,
+      fuses: event.params.fuses,
+      id: event.params.node,
+      name: event.params.name,
+      owner_id: event.params.owner
+    };
+    await context.WrappedDomain.set(wrappedDomain);
   }
-
-  let wrappedDomain: wrappedDomainEntity = {
-    domain_id: domainID,
-    expiryDate: event.params.expiry,
-    fuses: event.params.fuses,
-    id: event.params.node,
-    name: event.params.name,
-    owner_id: event.params.owner
-  };
-  context.WrappedDomain.set(wrappedDomain);
 });
 
-NameWrapperContract_OwnershipTransferred_loader(({ event: _, context }) => {
-  context.NameWrapperEventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY_2);
+NameWrapper.OwnershipTransferred.handlerWithLoader({
+  loader: async ({ event, context }) => {
+    const summary = await context.NameWrapperEventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY_2);
+    return { summary };
+  },
+  handler: async ({ event, context, loaderReturn }) => {
+    let { summary } = loaderReturn;
+
+    let currentSummaryEntity: NameWrapperEventsSummary =
+      summary ?? INITIAL_EVENTS_SUMMARY;
+
+    let nextSummaryEntity = {
+      ...currentSummaryEntity,
+      ownershipTransferredsCount:
+        currentSummaryEntity.ownershipTransferredsCount + BigInt(1)
+    };
+
+    let ownershipTransferredEntity: OwnershipTransferred = {
+      id: event.transaction.hash + event.logIndex.toString(),
+      previousOwner: event.params.previousOwner,
+      newOwner: event.params.newOwner,
+      eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY_2
+    };
+
+    await context.NameWrapperEventsSummary.set(nextSummaryEntity);
+    await context.OwnershipTransferred.set(ownershipTransferredEntity);
+  }
 });
 
-NameWrapperContract_OwnershipTransferred_handler(({ event, context }) => {
-  let summary = context.NameWrapperEventsSummary.get(
-    GLOBAL_EVENTS_SUMMARY_KEY_2
-  );
+NameWrapper.TransferBatch.handlerWithLoader({
+  loader: async ({ event, context }) => {
+    const summary = await context.NameWrapperEventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY_2);
+    return { summary };
+  },
+  handler: async ({ event, context, loaderReturn }) => {
+    let { summary } = loaderReturn;
 
-  let currentSummaryEntity: NameWrapperEventsSummaryEntity =
-    summary ?? INITIAL_EVENTS_SUMMARY;
+    let currentSummaryEntity: NameWrapperEventsSummary =
+      summary ?? INITIAL_EVENTS_SUMMARY;
 
-  let nextSummaryEntity = {
-    ...currentSummaryEntity,
-    ownershipTransferredsCount:
-      currentSummaryEntity.ownershipTransferredsCount + BigInt(1)
-  };
+    let nextSummaryEntity = {
+      ...currentSummaryEntity,
+      transferBatchsCount: currentSummaryEntity.transferBatchsCount + BigInt(1)
+    };
 
-  let ownershipTransferredEntity: OwnershipTransferredEntity = {
-    id: event.transactionHash + event.logIndex.toString(),
-    previousOwner: event.params.previousOwner,
-    newOwner: event.params.newOwner,
-    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY_2
-  };
+    let transferBatchEntity: TransferBatch = {
+      id: event.transaction.hash + event.logIndex.toString(),
+      operator: event.params.operator,
+      from: event.params.from,
+      to: event.params.to,
+      ids: event.params.ids,
+      values: event.params.values,
+      eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY_2
+    };
 
-  context.NameWrapperEventsSummary.set(nextSummaryEntity);
-  context.OwnershipTransferred.set(ownershipTransferredEntity);
+    await context.NameWrapperEventsSummary.set(nextSummaryEntity);
+    await context.TransferBatch.set(transferBatchEntity);
+  }
 });
 
-NameWrapperContract_TransferBatch_loader(({ event: _, context }) => {
-  context.NameWrapperEventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY_2);
-});
+NameWrapper.TransferSingle.handlerWithLoader({
+  loader: async ({ event, context }) => {
+    const summary = await context.NameWrapperEventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY_2);
+    return { summary };
+  },
+  handler: async ({ event, context, loaderReturn }) => {
+    let { summary } = loaderReturn;
 
-NameWrapperContract_TransferBatch_handler(({ event, context }) => {
-  let summary = context.NameWrapperEventsSummary.get(
-    GLOBAL_EVENTS_SUMMARY_KEY_2
-  );
+    let currentSummaryEntity: NameWrapperEventsSummary =
+      summary ?? INITIAL_EVENTS_SUMMARY;
 
-  let currentSummaryEntity: NameWrapperEventsSummaryEntity =
-    summary ?? INITIAL_EVENTS_SUMMARY;
+    let nextSummaryEntity = {
+      ...currentSummaryEntity,
+      transferSinglesCount: currentSummaryEntity.transferSinglesCount + BigInt(1)
+    };
 
-  let nextSummaryEntity = {
-    ...currentSummaryEntity,
-    transferBatchsCount: currentSummaryEntity.transferBatchsCount + BigInt(1)
-  };
+    let transferSingleEntity: TransferSingle = {
+      id: event.transaction.hash + event.logIndex.toString(),
+      operator: event.params.operator,
+      from: event.params.from,
+      to: event.params.to,
+      eventId: event.params.id,
+      value: event.params.value,
+      eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY_2
+    };
 
-  let transferBatchEntity: TransferBatchEntity = {
-    id: event.transactionHash + event.logIndex.toString(),
-    operator: event.params.operator,
-    from: event.params.from,
-    to: event.params.to,
-    ids: event.params.ids,
-    values: event.params.values,
-    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY_2
-  };
-
-  context.NameWrapperEventsSummary.set(nextSummaryEntity);
-  context.TransferBatch.set(transferBatchEntity);
-});
-
-NameWrapperContract_TransferSingle_loader(({ event: _, context }) => {
-  context.NameWrapperEventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY_2);
-});
-
-NameWrapperContract_TransferSingle_handler(({ event, context }) => {
-  let summary = context.NameWrapperEventsSummary.get(
-    GLOBAL_EVENTS_SUMMARY_KEY_2
-  );
-
-  let currentSummaryEntity: NameWrapperEventsSummaryEntity =
-    summary ?? INITIAL_EVENTS_SUMMARY;
-
-  let nextSummaryEntity = {
-    ...currentSummaryEntity,
-    transferSinglesCount: currentSummaryEntity.transferSinglesCount + BigInt(1)
-  };
-
-  let transferSingleEntity: TransferSingleEntity = {
-    id: event.transactionHash + event.logIndex.toString(),
-    operator: event.params.operator,
-    from: event.params.from,
-    to: event.params.to,
-    eventId: event.params.id,
-    value: event.params.value,
-    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY_2
-  };
-
-  context.NameWrapperEventsSummary.set(nextSummaryEntity);
-  context.TransferSingle.set(transferSingleEntity);
+    await context.NameWrapperEventsSummary.set(nextSummaryEntity);
+    await context.TransferSingle.set(transferSingleEntity);
+  }
 });
