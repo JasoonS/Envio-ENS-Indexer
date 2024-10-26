960c59f4d8018e030d3d0e98914657b7d92b0c1e
Jason <jason@envio.dev>
2024-10-26 08:51:06 +0200

    Tests compile again

--- src/tests/nameRegistered.ts
+++ src/tests/nameRegistered.ts
@@ -1,45 +1,56 @@
-/* import {
-  ENSRegistryWithFallback_NewOwner_createMockEvent,
-  ETHRegistrarController_NameRegistered_createMockEvent,
-  ETHRegistrarController_NameRegistered_processEvent,
-  MockDb
-} from "../../generated/src/TestHelpers.gen";
-import { Addresses } from "../../generated/src/bindings/Ethers.gen";
-// import { it } from "node:test";
-import { AccountEntity, DomainEntity, RegistrationEntity } from "../../generated/src/Types.gen";
-// import assert from "assert";
+import {
+
+  TestHelpers,
+  Account,
+  Domain,
+  Registration
+} from "generated";
+// import { TestHelpers, User } from "generated";
+const { MockDb, Addresses, ENSRegistryWithFallback,
+  ETHRegistrarController, } = TestHelpers;
+// import { Addresses } from "generated/src/bindings/Ethers.gen";
 import { expect } from 'chai';
 import { GRACE_PERIOD_SECONDS } from "../utils";
-import { t as mockDBType } from "../../generated/src/TestHelpers_MockDb.gen";
+import { t as mockDBType } from "generated/src/TestHelpers_MockDb.gen";
 
 import assert = require("assert")
 
-export const mockNameRegistered = (mockDbInitial: mockDBType): mockDBType => {
+export const mockNameRegistered = async (mockDbInitial: mockDBType): Promise<mockDBType> => {
   // Creating a mock event
-  const mockNameRegisteredEvent = ETHRegistrarController_NameRegistered_createMockEvent(
+  const mockNameRegisteredEvent = ETHRegistrarController.NameRegistered.createMockEvent(
     {
       name: "vibraciones",
       label:
         "0xd9a38024ac72f570a95923e9b046307c96b8e51187840f1bb758c3b6f9487fe1",
-      // owner: "0xd4416b13d2b3a9abae7acd5d6c2bbdbe25686401",
       owner: Addresses.defaultAddress,
       baseCost: BigInt(3775609184524047),
       premium: BigInt(0),
-      expires: BigInt(1696136927)
+      expires: BigInt(1696136927),
+      mockEventData: {
+        block: {
+          number: 1,
+          hash: "0x1",
+          timestamp: 1
+        },
+        transaction: {
+          hash: "0x1",
+          transactionIndex: 0
+        }
+      }
     }
   );
 
   // Processing the mock event on the mock database
-  const updatedMockDb = ETHRegistrarController_NameRegistered_processEvent({
+  const updatedMockDb = await ETHRegistrarController.NameRegistered.processEvent({
     event: mockNameRegisteredEvent,
     mockDb: mockDbInitial
   });
 
   // Expected entity that should be created
-  const exAccountEntity: AccountEntity = { id: Addresses.defaultAddress };
+  const exAccountEntity: Account = { id: Addresses.defaultAddress };
 
-  const exNameRegisteredEntity: DomainEntity = {
-    createdAt: BigInt(mockNameRegisteredEvent.blockTimestamp), isMigrated: true,
+  const exNameRegisteredEntity: Domain = {
+    createdAt: BigInt(mockNameRegisteredEvent.block.timestamp), isMigrated: true,
     labelName: "vibraciones",
     labelhash: mockNameRegisteredEvent.params.label,
     resolvedAddress_id: undefined,
@@ -61,14 +72,14 @@ export const mockNameRegistered = (mockDbInitial: mockDBType): mockDBType => {
     wrappedDomain_id: undefined,
   };
 
-  const exRegistrationEntity: RegistrationEntity = {
+  const exRegistrationEntity: Registration = {
     cost: mockNameRegisteredEvent.params.baseCost,
     domain_id:
       "0x000447a4d61a90d054b5e101f3c94c5c8819b7840e5a5442e07a21bc1c8c30b8",
     id: mockNameRegisteredEvent.params.label,
     registrant_id: exAccountEntity.id,
     expiryDate: mockNameRegisteredEvent.params.expires,
-    registrationDate: BigInt(mockNameRegisteredEvent.blockTimestamp)
+    registrationDate: BigInt(mockNameRegisteredEvent.block.timestamp)
   };
 
   // Getting the entity from the mock database
@@ -89,19 +100,29 @@ export const mockNameRegistered = (mockDbInitial: mockDBType): mockDBType => {
 
   return updatedMockDb;
 };
+
 describe("NameRegistered", () => {
-  it("test NameRegistered Success", () => {
+  it("test NameRegistered Success", async () => {
     // Initializing the mock database
     const mockDbInitial = MockDb.createMockDb();
 
-    const _mockNewOwnerEvent = ENSRegistryWithFallback_NewOwner_createMockEvent({
+    const _mockNewOwnerEvent = ENSRegistryWithFallback.NewOwner.createMockEvent({
       node: "0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae",
       label: "0xed23485acabad9a8dcfd46f6aa6d014d7de0336e053701a5dff28be996c1f78f",
-      owner: "0xd4416b13d2b3a9abae7acd5d6c2bbdbe25686401"
+      owner: "0xd4416b13d2b3a9abae7acd5d6c2bbdbe25686401",
+      mockEventData: {
+        block: {
+          number: 1,
+          hash: "0x1",
+          timestamp: 1
+        },
+        transaction: {
+          hash: "0x1",
+          transactionIndex: 0
+        }
+      }
     });
 
-    mockNameRegistered(mockDbInitial);
+    await mockNameRegistered(mockDbInitial);
   });
 });
-//test SubDomain create
- */
--- src/tests/newOwner.ts
+++ src/tests/newOwner.ts
@@ -1,38 +1,51 @@
-/* import {
-  ENSRegistryWithFallback_NewOwner_createMockEvent,
-  ENSRegistryWithFallback_NewOwner_processEvent,
-  MockDb
-} from "../../generated/src/TestHelpers.gen";
-import { Addresses } from "../../generated/src/bindings/Ethers.gen";
+import {
+  TestHelpers,
+  Account,
+  Domain
+} from "generated";
+const { MockDb, Addresses, ENSRegistryWithFallback } = TestHelpers;
+// import { Addresses } from "generated/src/bindings/Ethers.gen";
 import { expect } from 'chai';
-import { AccountEntity, DomainEntity } from "../../generated/src/Types.gen";
 import { mockNameRegistered } from "./nameRegistered";
+import { EventFunctions_mockEventData, EventFunctions_MockTransaction_t } from "generated/src/TestHelpers.gen";
 
-it("test SubDomain Creation Success", () => {
+it("test SubDomain Creation Success", async () => {
   // Initializing the mock database
   const mockDbInitial = MockDb.createMockDb();
 
   // Initializing values for mock event
-  const nameRegisteredMockDb = mockNameRegistered(mockDbInitial);
+  const nameRegisteredMockDb = await mockNameRegistered(mockDbInitial);
 
-  const mockNewOwnerEvent = ENSRegistryWithFallback_NewOwner_createMockEvent({
+  const mockNewOwnerEvent = ENSRegistryWithFallback.NewOwner.createMockEvent({
     node: "0x000447a4d61a90d054b5e101f3c94c5c8819b7840e5a5442e07a21bc1c8c30b8",
     label: "0x6bc67ae9b64c1f7ee4133c8c6ab43ec6b2c3bba0509b6cd95f2c88a0da91fe21",
-    owner: Addresses.defaultAddress
+    owner: Addresses.defaultAddress,
+    mockEventData: {
+      block: {
+        number: 1,
+        hash: "0x1",
+        timestamp: 1,
+      },
+      transaction: {
+        hash: "0xa",
+        transactionIndex: 0,
+      },
+      logIndex: 0,
+    }
   });
 
   // Processing the mock event on the mock database
-  const updatedMockDb = ENSRegistryWithFallback_NewOwner_processEvent({
+  const updatedMockDb = await ENSRegistryWithFallback.NewOwner.processEvent({
     event: mockNewOwnerEvent,
     mockDb: nameRegisteredMockDb
   });
 
   // Expected entity that should be created
-  const exAccountEntity: AccountEntity = { id: Addresses.defaultAddress };
+  const exAccountEntity: Account = { id: Addresses.defaultAddress };
 
-  const exNameRegisteredEntity: DomainEntity = {
+  const exNameRegisteredEntity: Domain = {
     baseCost: undefined,
-    createdAt: BigInt(mockNewOwnerEvent.blockTimestamp),
+    createdAt: BigInt(mockNewOwnerEvent.block.timestamp),
     expiryDate: undefined,
     isMigrated: true,
     labelName: undefined,
@@ -50,7 +63,6 @@ it("test SubDomain Creation Success", () => {
     resolver_id: undefined,
     registration_id: undefined,
     wrappedDomain_id: undefined,
-
   };
 
   // Getting the entity from the mock database
@@ -60,10 +72,8 @@ it("test SubDomain Creation Success", () => {
 
   // TODO Check Parent SubdomainCount is 1
   // TODO Reverse Lookups on Account for Domains?
-  // TODO Reverse Lookups on Account for Domains?
   // Asserting that the entity in the mock database is the same as the ex entity
   expect(domEntity).to.deep.equal(exNameRegisteredEntity);
 });
 
 //test SubDomain create
- */
